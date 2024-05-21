cd2f49955f2a9a6251eab66e93888ddf65e67a57
Richele <168489035+devrichele@users.noreply.github.com>
2024-05-21 23:05:09 +0000

    continuar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
 <div className='body'> 
-<h1 className=''> my finance </h1>
-<button>Nova Transação </button>
-<div class="card">
-      <div class="card-item">Entradas</div>
-      <div class="card-item">Saídas</div>
-      <div class="card-item">Total</div>
+<h1 className='empresa'> my finance </h1>
+<button>Nova Transação</button>
+<div className="card">
+      <div className="card-1">Entradas</div>
+      <div className="card-2">Saídas</div>
+      <div className="card-3">Total</div>
 
 <p> Título Valor Categoria Data </p>
 </div>
