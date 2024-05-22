96d0fd29e210295f7f5627d2ff9d57ba50491494
Richele <168489035+devrichele@users.noreply.github.com>
2024-05-22 23:34:37 +0000

    CONTINEUNAODESISTA

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import "./App.css"
 
 function App() {
   return (
-<div className='body'> 
-<h1 className='empresa'> my finance </h1>
-<button>Nova Transação</button>
-<div className="card">
-      <div className="card-1">Entradas</div>
-      <div className="card-2">Saídas</div>
-      <div className="card-3">Total</div>
+<div className='body'>
+  <div className='empresa'>
+    <h1 className='logo'>my finance</h1>
+    <button>Nova Transação</button>
+   </div>
+    <div ClasseName="cards">
 
-<p> Título Valor Categoria Data </p>
+  </div>
 </div>
-</div>
+
   )
 }
 
