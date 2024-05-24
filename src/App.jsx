c5af425e3f38f8511fa19034fe67b8dcf40b02e2
Richele <168489035+devrichele@users.noreply.github.com>
2024-05-24 23:38:44 +0000

    App.jsx-atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,19 @@ function App() {
   return (
 <div className='body'>
   <div className='empresa'>
-    <h1 className='logo'>my finance</h1>
+    <h1 className='logo'>Rich Finance</h1>
     <button>Nova Transação</button>
-  </div>
-  <div className='cards'>
-    <div className='card'>
-      <h2>Entrada</h2>
-      <p>$0.00</p>
     </div>
+    <div className='cards'>
     <div className='card'>
-      <h2>Saída</h2>
-      <p>$0.00</p>
-    </div>
-    <div className='card'>
-      <h2>Total</h2>
-      <p>$0.00</p>
+      <div className="container-card-icon">
+
+      </div>
+      <div className="transacoes">
+        <p className="texto">Entradas</p>
+        <p className="valor"> R$ 1020,50</p>
+      </div>
+
     </div>
   </div>
 </div>
