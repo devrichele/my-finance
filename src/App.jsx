f0a9d1e119d5f35c6c4dfbf7e69e1a465ed4e5ca
Richele <168489035+devrichele@users.noreply.github.com>
2024-05-24 00:58:18 +0000

    CONTINUAR E ESTUDAR

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,23 @@ function App() {
   <div className='empresa'>
     <h1 className='logo'>my finance</h1>
     <button>Nova Transação</button>
-   </div>
-    <div ClasseName="cards">
-
+  </div>
+  <div className='cards'>
+    <div className='card'>
+      <h2>Entrada</h2>
+      <p>$0.00</p>
+    </div>
+    <div className='card'>
+      <h2>Saída</h2>
+      <p>$0.00</p>
+    </div>
+    <div className='card'>
+      <h2>Total</h2>
+      <p>$0.00</p>
+    </div>
   </div>
 </div>
+
 
   )
 }
