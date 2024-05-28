466df3a0d97aea3377d3b3b64564de3401cc6110
Richele <168489035+devrichele@users.noreply.github.com>
2024-05-28 22:58:13 +0000

    recriando props card

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import Card from "./components/cards"
 
 function App() {
   return (
-<div className='body'>
-  <div className='empresa'>
-    <h1 className='logo'>Rich Finance</h1>
-    <button>Nova Transação</button>
-    </div>
-    <div className='cards'>
-    <Card texto ={"Entradas"} valor={"450"}/>
-    <Card texto ={"Saídas"} valor={"250"}/>
-    <Card texto ={"Total"} valor={"700"}/>
+    <div className='body'>
+      <div className='empresa'>
+        <h1 className='logo'>Rich Finance</h1>
+        <button>Nova Transação</button>
+      </div>
+      <div className='cards'>
+        <Card texto ={"Entradas"} valor={"450,00"}/>
+        <Card texto ={"Saídas"} valor={"250,00"}/>
+        <Card texto ={"Total"} valor={"700,00"}/>
 
-   </div>
-</div>
+      </div>
+      <div className="card2">
+        <Card texto ={"Escola"} valor={"900,00"}/>
+        <Card texto ={"Eletropaulo"} valor={"100,00"}/>
+        <Card texto ={"Sabesp"} valor={"150,00"}/>
+
+      </div>
+    </div>
 
 
   )
