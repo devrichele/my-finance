0d9b7e39e21a9c66a9233690b2d46c70a7d22336
Richele <168489035+devrichele@users.noreply.github.com>
2024-06-13 22:54:48 +0000

    renderizando modal usando condição

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css"
 import Card from "./components/cards"
-
+import Modelo from "./components/modelo"
 
 function App() {
+
+  const isOpenModelo = true
+
+  function alertaMensagem(name){
+    alert (name)
+  }
+
   return (
     <div className='body'>
       <div className='empresa'>
         <h1 className='logo'>Rich Finance</h1>
-        <button>Nova Transação</button>
+        <button onClick={() => alertaMensagem ("Brenda Richele")} > 
+          Nova Transação
+        </button>
       </div>
       <div className='cards'>
         <Card texto ={"Entradas"} valor={"450,00"}/>
@@ -22,6 +31,7 @@ function App() {
         <Card type={"baixo"} mudar={50} texto ={"Spotify"} valor={"Domingo, 6:00 AM"}/>
         
       </div>
+      {isOpenModelo && <Modelo />}
     </div>
 
 
---
+++ src/components/modelo/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.css"
+
+export default function Modelo (){
+    return(
+    <div className="body-teste">
+        <h1>Modelo</h1>
+    </div>
+    )
+}
