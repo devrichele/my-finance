4ea53f3fd1fb4e4e1469d39e9b3c3b14f1d0d3a9
Richele <168489035+devrichele@users.noreply.github.com>
2024-06-18 23:07:04 +0000

    aula de revisão, components, input e usestate

--- src/components/modelo/index.jsx
+++ src/components/modelo/index.jsx
@@ -1,9 +1,37 @@
+import EntradaCadastro from "../entrada"
 import "./styles.css"
 
-export default function Modelo ({onClickModelo}){
+
+
+export default function Modelo (){
+    
     return (
-    <div className="body-teste" onClick={onClickModelo}>
-        <h1>Modelo</h1>
+    <div className="body-modelo">
+      <div className="conteudoModelo">
+        <div>
+            
+        </div>
+        <form>
+            <h1> Cadastrar Transação </h1>
+            <div>
+                <EntradaCadastro />
+
+                <EntradaCadastro />
+
+            </div>
+        </form>
+      </div>
     </div>
     )
 }
+
+
+// import "./styles.css"
+
+// export default function Modelo ({onClickModelo}){
+//     return (
+//     <div className="body-teste" onClick={onClickModelo}>
+//         <h1>Modelo</h1>
+//     </div>
+//     )
+// }
